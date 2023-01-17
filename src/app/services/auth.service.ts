25fa7081b4d1928cf47f23ecd620acf6acce192c
MUHAMMED FURKAN USLU <furkan42uslu@gmail.com>
2023-01-17 23:49:34 +0300

    170123

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginModel } from '../models/entities/loginModel';
+import { RegisterModel } from '../models/entities/registerModel';
+import { SingleResponseModel } from '../models/responses/singleResponseModel';
+import { TokenModel } from '../models/tokenModel';
+import { LocalStorageService } from './local-storge.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  apiUrl = 'https://localhost:44389/api/';
+
+  constructor(
+    private httpClient: HttpClient,
+    private localStorageService: LocalStorageService
+  ) {}
+
+  login(loginModel: LoginModel): Observable<SingleResponseModel<TokenModel>> {
+    let newPath = this.apiUrl + 'auth/login';
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(
+      newPath,
+      loginModel
+    );
+  }
+
+  register(
+    registerModel: RegisterModel
+  ): Observable<SingleResponseModel<TokenModel>> {
+    let newPath = this.apiUrl + 'auth/register';
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(newPath,registerModel)
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.localStorageService.getToken();
+ }
+}
